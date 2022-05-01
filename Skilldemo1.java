6fb05148847281b50846f637db6e71e5771efcf9
KaifYang <103291661+KaifYang@users.noreply.github.com>
2022-05-01 01:45:23 -0700

    Skilldemo1

--- Skilldemo1.java
+++ Skilldemo1.java
@@ -1,3 +1,5 @@
 class Skilldemo1 {
-
+    public static int multiply(int f, int s) {
+        return f*s;
+    }
 }
---
+++ Skilldemo1Test.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.*;
+
+public class Skilldemo1Test {
+    @Test
+    public void Testmultiply() {
+        assertEquals(5, Skilldemo1.multiply(2, 5));
+    }
+}
